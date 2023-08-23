488138edaf85b3d4db3e2e8b23e36bcd0edd53c2
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-08-23 13:15:36 +0200

    full test fix

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -1,33 +1,31 @@
 const { Shop, Item } = require("../src/gilded_rose.js");
 
 describe("Gilded Rose", function () {
-  // it("full test", () => {
-  //   const items = [
-  //     new Item("+5 Dexterity Vest", 10, 20),
-  //     new Item("Aged Brie", 2, 0),
-  //     new Item("Elixir of the Mongoose", 5, 7),
-  //     new Item("Sulfuras, Hand of Ragnaros", 0, 80),
-  //     new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-  //     new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-  //     new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-  //     new Item("Backstage passes to a TAFKAL80ETC concert", 5, 39),
+  it("full test", () => {
+    const items = [
+      new Item("+5 Dexterity Vest", 10, 20),
+      new Item("Aged Brie", 2, 0),
+      new Item("Elixir of the Mongoose", 5, 7),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 39),
+      new Item("Conjured Mana Cake", 3, 6),
+    ];
 
-  //     // This Conjured item does not work properly yet
-  //     new Item("Conjured Mana Cake", 3, 6),
-  //   ];
+    const days = Number(process.argv[2]) || 2;
+    const gildedRose = new Shop(items);
 
-  //   const days = Number(process.argv[2]) || 2;
-  //   const gildedRose = new Shop(items);
-
-  //   for (let day = 0; day < days; day++) {
-  //     console.log(`\n-------- day ${day} --------`);
-  //     console.log("name, sellIn, quality");
-  //     items.forEach((item) =>
-  //       console.log(`${item.name}, ${item.sellIn}, ${item.quality}`)
-  //     );
-  //     gildedRose.updateQuality();
-  //   }
-  // });
+    for (let day = 0; day < days; day++) {
+      console.log(`\n-------- day ${day} --------`);
+      console.log("name, sellIn, quality");
+      items.forEach((item) =>
+        console.log(`${item.name}, ${item.sellIn}, ${item.quality}`)
+      );
+      gildedRose.updateQuality();
+    }
+  });
 
   // Test if Aged Brie quality increases by 1 when sellIn--
   it("Aged Brie quality increases with age", () => {
@@ -69,12 +67,28 @@ describe("Gilded Rose", function () {
     expect(items[0].quality).toBe(9);
   });
 
+  //Test if default items decreases by 2 when sellIn < 0
+  it("Default item decreases with age", () => {
+    const items = [new Item("Cabbage", -2, 10)];
+    const gildedRose = new Shop(items);
+    gildedRose.updateQuality();
+    expect(items[0].quality).toBe(8);
+  });
+
   //Test if items whose quality go up cap at 50
   it("Quality never goes past 50", () => {
     const items = [new Item("Aged Brie", 3, 50)];
     const gildedRose = new Shop(items);
     gildedRose.updateQuality();
     expect(items[0].quality).toBe(50);
+  });
+
+  //Test if items whose quality are never negative
+  it("Quality never goes past 50", () => {
+    const items = [new Item("Celery", 2, 0)];
+    const gildedRose = new Shop(items);
+    gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
   });
 
   //Test if Sulfuras quality is always 80
@@ -84,5 +98,21 @@ describe("Gilded Rose", function () {
     gildedRose.updateQuality();
     expect(items[0].quality).toBe(80);
     expect(items[0].sellIn).toBe(null);
+  });
+
+  //Test if Conjured item loses quality twice as fast
+  it("Quality of Conjured item decreases .2x", () => {
+    const items = [new Item("Conjured Cabbage", 10, 30)];
+    const gildedRose = new Shop(items);
+    gildedRose.updateQuality();
+    expect(items[0].quality).toBe(28);
+  });
+
+  //Test if Conjured item loses quality x2 twice as fast when sellIn < 0
+  it("Quality of Conjured item decreases .2x", () => {
+    const items = [new Item("Conjured Cabbage", -2, 10)];
+    const gildedRose = new Shop(items);
+    gildedRose.updateQuality();
+    expect(items[0].quality).toBe(6);
   });
 });
--- js-jasmine/src/gilded_rose.js
+++ js-jasmine/src/gilded_rose.js
@@ -24,7 +24,9 @@ class Shop {
         item.sellIn--;
       },
       "Backstage passes": (item) => {
-        if (item.sellIn <= 5 && item.quality < 50) {
+        if (item.sellIn < 0) {
+          item.quality = 0;
+        } else if (item.sellIn <= 5 && item.quality < 50) {
           item.quality += 3;
         } else if (item.sellIn <= 10 && item.quality < 50) {
           item.quality += 2;
@@ -32,6 +34,19 @@ class Shop {
           item.quality++;
         }
         if (item.quality > 50) {
+          item.quality = 50;
+        }
+        item.sellIn--;
+      },
+      Conjured: (item) => {
+        if (item.sellIn < 0) {
+          item.quality -= 4;
+        } else if (item.sellIn > 0) {
+          item.quality -= 2;
+        }
+        if (item.quality <= 0) {
+          item.quality = 0;
+        } else if (item.quality > 50) {
           item.quality = 50;
         }
         item.sellIn--;
@@ -44,16 +59,25 @@ class Shop {
       const item = this.items[i];
       const rule =
         this.itemRules[item.name] ||
+        (item.name.includes("Conjured")
+          ? this.itemRules["Conjured"]
+          : undefined) ||
+        (item.name.includes("Sulfuras")
+          ? this.itemRules["Sulfuras"]
+          : undefined) ||
+        (item.name.includes("Backstage passes")
+          ? this.itemRules["Backstage passes"]
+          : undefined) ||
         ((item) => {
-          if (item.quality < 0) {
-            item.quality = 0;
-          } else if (item.quality > 50) {
-            item.quality = 50;
-          }
           if (item.sellIn < 0) {
             item.quality -= 2;
           } else if (item.sellIn > 0) {
             item.quality--;
+          }
+          if (item.quality <= 0) {
+            item.quality = 0;
+          } else if (item.quality > 50) {
+            item.quality = 50;
           }
           item.sellIn--;
         });
